848a94b075010f5149014182e7ff4de8dfb3d9e9
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2018-03-22 01:25:39 -0400

    Head: Enables Roboto font.

--- components/head.php
+++ components/head.php
@@ -3,6 +3,6 @@
     <link rel="stylesheet" href="css/custom.css">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/sidebar.css">
-    <!-- <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link href="css/prism.css" rel="stylesheet" />
 </head>
