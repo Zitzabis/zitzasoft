2466ea4c99789257e505b45ff94b89d5c112443c
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2018-03-21 20:44:33 -0400

    Adds starter pages for About and Contact.

---
+++ contact.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <?php
+        include_once("components/head.php");
+        include_once("components/scripts.php");
+    ?>
+    <body>
+        <?php include_once("components/navBarIcon.php"); ?>
+        <main>
+            <div class="pageContent centerContent"> 
+                <?php include_once("components/nav.php"); ?>
+                <div>
+                    <!-- Content Here -->
+                </div>
+            </div>
+        </main>
+        <?php include_once("components/sidebar.php"); ?>
+    </body>
+</html>
