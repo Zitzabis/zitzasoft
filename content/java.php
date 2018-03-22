13bda052fe437c21a51251ea3d15e1adb35a667c
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2018-03-22 01:26:01 -0400

    Java: Adds Java page.

---
+++ content/java.php
@@ -0,0 +1,29 @@
+<div class="panel">
+    <div class="row">
+        <div class="col text-center">
+            <h3>Java Projects</h3>
+        </div>
+    </div>
+    <div class="row rowPad">
+        <div class="col panel">
+            <div>
+                <h5>City of Walhalla</h5>
+                Status: <span class="text-success">Completed</span>
+                <br>
+                Link: <a href="https://play.google.com/store/apps/details?id=zitzasoft.com.wbclibrary">Google Play Store App Page</a>
+            </div>
+            <div class="rowPad">
+                Contracted by the Wesleyan Bible College of Papua New Guinea to develop an app to help enable students in a third-world setting access the library catalog.
+                <br>
+                The country is heavily prone to power outages that can last for days, which renders the C# application I developed useless. Thus a mobile version was created.
+                <br>
+                <br>
+                Images:
+                <br>
+                <img src="https://lh3.googleusercontent.com/jK27Q3FN9xHr4UlP2QzFAWVZ4IuZGxdn8AkMoNLsQBPyZKsZaGsBqmctzPAgP9v_JA=w1920-h959" class="img-fluid" style="width: 400px">
+                <img src="https://lh3.googleusercontent.com/5NkbTZqd0rbe3rfXSDK8OHYMKsoItWnlvT41GGz_d3f74w2UNhO-ubHRF6pyH8rHisA=w1920-h959" class="img-fluid" style="width: 400px">
+                <img src="https://lh3.googleusercontent.com/wXh8LrXfmkB8BETW8qB31JITrRd2cZHdnu3LjZlsYVRMXpiWRHoRA9MIseqesHEZlL4=w1920-h959" class="img-fluid" style="width: 400px">
+            </div>
+        </div>
+    </div>
+</div>
