5a6c4f24bd892198e99e3e2022911646135f4463
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2018-03-21 20:56:38 -0400

    PHP: Adds PHP page content.

---
+++ content/PHP.php
@@ -0,0 +1,50 @@
+<div class="panel">
+    <div class="row">
+        <div class="col text-center">
+            <h3>PHP Projects</h3>
+        </div>
+    </div>
+    <div class="row rowPad">
+        <div class="col panel">
+            <div>
+                <h5>City of Walhalla</h5>
+                Status: <span class="text-success">Current</span>
+                <br>
+                Link: <a href="http://walhalla.brycorpsolutions.com/">http://walhalla.brycorpsolutions.com/</a>
+            </div>
+            <div class="rowPad">
+                Employed by Brycrop LLC to be the lead web developer and team leader for the contract with the City of Walhalla.
+                <br>
+                Features:
+                <ul class="listPad">
+                    <li>Custom CMS built from the ground up in PHP/JavaScript/jQuery/MySQL</li>
+                    <li>Front-end and back-end web development for custom features</li>
+                    <li>Database driven for dynamic content</li>
+                    <li>Content changes tracked by custom revision system for emergency rollbacks</li>
+                    <li>Local business advertising system</li>
+                    <li>Customizable department pages to meet department needs</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="row rowPad">
+        <div class="col panel">
+            <div>
+                <h5>Collins Upstate Electrical</h5>
+                Status: <span class="text-success">Current</span>
+                <br>
+                Link: <a href="http://collinselectricalsc.com/">http://collinselectricalsc.com/</a>
+            </div>
+            <div class="rowPad">
+                Freelance job to develop a website for Collins Upstate Electrical.
+                <br>
+                Features:
+                <ul class="listPad">
+                    <li>Custom CMS built from the ground up in PHP/JavaScript/MySQL</li>
+                    <li>Front-end and back-end web development for custom features</li>
+                    <li>Database driven for dynamic content</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
